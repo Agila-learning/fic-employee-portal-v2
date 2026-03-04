dc84a8c5b6cff330f835295f52af79f0f7a2475e
Agila-learning <agilaagi0922@gmail.com>
2026-03-04 23:24:12 +0530

    Fix critical double-hashing bug in User model

--- backend_v2/models/User.js
+++ backend_v2/models/User.js
@@ -32,7 +32,7 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
 // Encrypt password using bcrypt
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        next();
+        return next();
     }
 
     const salt = await bcrypt.genSalt(10);
